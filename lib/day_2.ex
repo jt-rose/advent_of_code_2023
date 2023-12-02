6467c28dedfe2937031f6ab3be1fad378808396e
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-12-02 02:13:35 -0500

    refactor and optimize code

--- lib/day_2.ex
+++ lib/day_2.ex
@@ -4,9 +4,12 @@ defmodule Day2 do
 
   Link: https://adventofcode.com/2023/day/2
   """
-  @blue_count_regex ~r/(\d+) blue/
-  @red_count_regex ~r/(\d+) red/
-  @green_count_regex ~r/(\d+) green/
+
+  @regex_map %{
+    blue: ~r/(\d+) blue/,
+    red: ~r/(\d+) red/,
+    green: ~r/(\d+) green/
+  }
 
   @type max_colors :: %{blue: integer(), red: integer(), green: integer()}
 
@@ -28,37 +31,51 @@ defmodule Day2 do
     |> Enum.sum()
   end
 
-  def map_valid_game_id(game, max_colors) do
+  # define default color values
+  def map_valid_game_id(game, max_colors, colors \\ [:blue, :red, :green])
+
+  # define success case and extract game id
+  def map_valid_game_id(game, _max_colors, []) do
     [_, game_id] = Regex.run(~r/Game (\d+)/, game)
-    blue_count = map_color_results(game, @blue_count_regex)
-    red_count = map_color_results(game, @red_count_regex)
-    green_count = map_color_results(game, @green_count_regex)
+    String.to_integer(game_id)
+  end
 
-    invalid_blues = is_invalid_game?(blue_count, Map.get(max_colors, :blue))
-    invalid_reds = is_invalid_game?(red_count, Map.get(max_colors, :red))
-    invalid_greens = is_invalid_game?(green_count, Map.get(max_colors, :green))
+  # check for valid color amount, abort with 0 if invalid
+  def map_valid_game_id(game, max_colors, [color | colors]) do
+    regex = Map.get(@regex_map, color)
+    max_allowed = Map.get(max_colors, color)
+    count = map_color_results(game, regex)
 
-    if invalid_blues or invalid_reds or invalid_greens do
+    if is_invalid_game?(count, max_allowed) do
       0
     else
-      String.to_integer(game_id)
+      map_valid_game_id(game, max_colors, colors)
     end
   end
 
-  def get_minimum_req_power(game) do
-    blue_count = map_color_results(game, @blue_count_regex) |> Enum.max()
-    red_count = map_color_results(game, @red_count_regex) |> Enum.max()
-    green_count = map_color_results(game, @green_count_regex) |> Enum.max()
-
-    blue_count * red_count * green_count
-  end
-
+  # extract integer counts for each color in a game
   def map_color_results(game, regex) do
     Regex.scan(regex, game)
     |> Enum.map(fn [_, count] -> String.to_integer(count) end)
   end
 
+  # check if invalid color amount present
   def is_invalid_game?(color_results, max) do
     Enum.any?(color_results, fn x -> x > max end)
+  end
+
+  # get power of minimally required amount for each color
+  def get_minimum_req_power(game) do
+    %{
+      blue: blue_regex,
+      red: red_regex,
+      green: green_regex
+    } = @regex_map
+
+    blue_count = map_color_results(game, blue_regex) |> Enum.max()
+    red_count = map_color_results(game, red_regex) |> Enum.max()
+    green_count = map_color_results(game, green_regex) |> Enum.max()
+
+    blue_count * red_count * green_count
   end
 end
