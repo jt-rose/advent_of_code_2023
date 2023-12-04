8fffb56e7a9d597994e6ded98bd19d525b3d6c70
Jeff Rose <50463882+jt-rose@users.noreply.github.com>
2023-12-03 22:19:01 -0500

    remove unused function

--- test/day_2_test.exs
+++ test/day_2_test.exs
@@ -17,8 +17,4 @@ defmodule Day2Test do
   test "part 2 is correct" do
     assert Day2.solve_part_2(@sample_text) == 70387
   end
-
-  def sample_2() do
-    "Game 1: 4 green, 3 blue, 11 red; 7 red, 5 green, 10 blue; 3 green, 8 blue, 8 red; 4 red, 12 blue; 15 red, 3 green, 10 blue"
-  end
 end
